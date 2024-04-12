2f26ce16016ff20432d6e38220e78038eb9c5791
Maxwell Odoom <maxwell.odoom@amalitech.com>
2024-04-12 13:10:30 +0100

    added the app's entry point file

---
+++ src/main/java/com/umaxcode/restaurantapp/UserAuthJWTApplication.java
@@ -0,0 +1,14 @@
+package com.umaxcode.restaurantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthJWTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthJWTApplication.class, args);
+
+	}
+
+}
